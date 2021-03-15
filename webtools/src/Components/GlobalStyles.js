fb613716628c4affd1798906cdb8ff830ccdb293
SUNG-HWAN-LIM <77113306+SUNG-HWAN-LIM@users.noreply.github.com>
2021-03-15 13:16:03 +0900

    수정

--- webtools/src/Components/GlobalStyles.js
+++ webtools/src/Components/GlobalStyles.js
@@ -12,9 +12,10 @@ const globalStyles = createGlobalStyle`
     }
     body{
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-size:13px;
+        font-size:12px;
+        background-color:rgba(20, 20, 20, 1);
         color:white;
-        padding-top:50px;
+        padding-top:200px;
     }
 `;
 
